f8ff943b8539fae0e8d13a954abe4d3d4799dd96
keniajin <keniajin@gmail.com>
2019-09-12 08:08:53 +0300

    added the readme file and install packages file

---
+++ README.md
@@ -0,0 +1,12 @@
+# YES Monitoring and Evaluation Framework
+
+This framework shows ME graphs for the YES project
+
+
+## Data Sources
+
+
+## Data Summarised
+
+
+## 
---
+++ packages_install.R
@@ -0,0 +1,28 @@
+
+## the packages being used in YES dashboard
+packages_install <-c("tidyverse","flexdashboard","ggpubr","leaflet",
+                 "devtools","sf", "shinyjs", "rgdal","maptools", "rgeo", "readxl",
+                 "stringi", "RColorBrewer", "plotly") 
+
+
+## install packages from bioconductor or CRAN
+for(i in 1:length(packagesLoad)){
+  if(require(packagesLoad[i], character.only = TRUE)==FALSE){
+     install.packages(packagesLoad[i]);
+    
+    #install bioconductor packages
+    print(paste0("missing_", packagesLoad[i]))
+    
+    if(require(packagesLoad[i], character.only = TRUE)==FALSE) {
+      source("http://bioconductor.org/biocLite.R")
+      biocLite(packagesLoad[i],suppressUpdates=T,ask=F)
+      print(paste0("missing_", packagesLoad[i]))
+      
+    }
+}## close if
+  
+}
+
+## indpedent packages from shiny
+devtools::install_github("hadley/shinySignals")
+devtools::install_github("jcheng5/bubbles")
